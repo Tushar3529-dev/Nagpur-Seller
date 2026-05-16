e455d16ca63c6f79571776e2207dc384acdd5193
Tushar3529-dev <tusharyadav3529@gmail.com>
2026-05-16 12:57:41 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/nagpurmart/seller/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.nagpurmart.seller
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/config/constant.dart
@@ -0,0 +1,9 @@
+class AppConstants {
+  // Api related variables
+
+  static String domainUrl = 'https://admin.nagpurmart.in';
+
+  static String appName = 'Nagpur Mart Seller ';
+
+  static String fontFamily = 'Inter';
+}
